9b4410db98e1aaf9064018f0d95ad4225b7f5938
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-04-04 18:52:34 -0400

    complete

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -3,6 +3,8 @@ import '../App.css'
 import axios from 'axios';
 import FriendsList from './friendsList';
 import Form from './Form';
+import UpdateForm from './UpdateForm';
+import {Route} from 'react-router-dom';
 
 export default class Friends extends React.Component{
     constructor(){
@@ -27,26 +29,47 @@ export default class Friends extends React.Component{
         axios.post('http://localhost:5000/friends', fri)
         .then(res=> {
           console.log(res)
+          
         })
         .catch(err=> {
           console.log(err)
         })
       }
 
+      putMessage = (id, updatedFriend) => {
+          axios.put(`http://localhost:5000/friends/${id}`, updatedFriend)
+          .then(res=> console.log(res))
+          .catch(err=> console.log(err))
+      }
+
+      deleteFriend = (id) => {
+
+        axios.delete(`http://localhost:5000/friends/${id}`)
+        .then(res=> {
+            this.setState({ datas: res.data })
+            console.log(res)
+        })
+        .catch(err=> console.log(err))
+      }
+
+      
     
     render(){
         
         return(
             <div>
-                <Form postM={this.postMessage}/>
+                <h1>Friends List</h1>
+                <Route path="/add" render={()=> <Form postM={this.postMessage} />}/>
+                <Route path="/update" render={() => <UpdateForm putM={this.putMessage} />} />
                 <div className="container">
                     {this.state.datas.map(item => (
-                    <FriendsList key={item.id} name={item.name} 
-                    age={item.age} email={item.email}
-                    />
+                    <FriendsList key={item.id} name={item.name} id={item.id}
+                    age={item.age} email={item.email} deleteFriend={this.deleteFriend}
+                    /> 
                     
                     ))}
                 </div>
+                
             </div>
         )
     }
--- friends/src/components/friendsList.js
+++ friends/src/components/friendsList.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import '../App.css'
+import {Link} from 'react-router-dom';
 
-export default function FriendsList(props){
+ const FriendsList =props => {
     
+    const handleClick = e => {
+        props.deleteFriend(props.id);
+    }
+    
+
     return(        
         <div className="card">
             <h1>{props.name}</h1>
             <p><strong>Age: </strong>{props.age}</p>
             <p><strong>Email: </strong>{props.email}</p>
+            <button onClick={handleClick}>Delete</button>
+            <Link to="/update"><button>Update</button></Link>
+            <Link to="/add"><button>Add</button></Link>
         </div>
      
     )
 }
+
+export default FriendsList;
